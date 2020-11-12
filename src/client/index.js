b1c723066530f8a32c365c589ae64e2a029cbb66
Irina Kramer <irakramer@gmail.com>
2020-11-11 22:30:24 -0800

    added twitter tags

--- src/client/index.js
+++ src/client/index.js
@@ -8,6 +8,7 @@ import './styles/form.scss'
 import './styles/header.scss'
 import './styles/message.scss'
 import './images/logo.png'
+import './images/evaluate-news-screenshot.png'
 
 export {
     checkForUrl,

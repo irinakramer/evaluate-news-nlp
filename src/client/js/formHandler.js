9efc04f525c1414f735f1c010272ca93a6306082
Irina Kramer <irakramer@gmail.com>
2020-11-17 17:55:46 -0800

    fix: Fetch relative URL

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -23,7 +23,7 @@ async function handleSubmit(event) {
 
     // check if user input is valid
     if (Client.checkForUrl(formText)) {
-        await fetch('https://evaluate-news-nlp-17.netlify.app/article', {
+        await fetch('/article', {
             method: 'POST',
             cache: 'no-cache',
             credentials: 'same-origin',

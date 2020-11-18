4eb869e066c080b2f19cd40dd15ec9503d6cd4d3
Irina Kramer <irakramer@gmail.com>
2020-11-17 21:04:41 -0800

    fix: Add localhost to client

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -23,7 +23,7 @@ async function handleSubmit(event) {
 
     // check if user input is valid
     if (Client.checkForUrl(formText)) {
-        await fetch('/article', {
+        await fetch('http://localhost:8081/article', {
             method: 'POST',
             cache: 'no-cache',
             credentials: 'same-origin',
